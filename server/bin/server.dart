ee9201a5b4ab8a74cc73a5b69490c933cae73c9f
yury kornienko <geo97it@gmail.com>
2019-06-01 22:05:04 +0300

    fix port

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
 import 'package:server/server.dart';
 
-Future<void> main() async => await run();
+Future<void> main() async => await run(
+  port: int.parse(Platform.environment['PORT']),
+  isolatesCount: 1,
+);
