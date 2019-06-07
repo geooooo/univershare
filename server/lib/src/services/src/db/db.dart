bf98421adf8cd00dd27069ddd39d08b79df7ac02
yury kornienko <geo97it@gmail.com>
2019-06-07 13:21:59 +0300

    update db api

--- server/lib/src/services/src/db/db.dart
+++ server/lib/src/services/src/db/db.dart
@@ -6,6 +6,8 @@ import 'src/event_table.dart';
 import 'src/presentation_table.dart';
 import 'src/user_table.dart';
 import 'src/message_table.dart';
+import 'src/presenter_table.dart';
+import 'src/listener_table.dart';
 
 class Db {
 
@@ -16,128 +18,126 @@ class Db {
   Future<void> createEvent(
       String eventId,
       String eventName,
-      String creatorName,
+      String presenterName,
       String presentationUrl,
   ) async {
-//    final queryCreateUser = Query<UserTable>(managedContext)
-//      ..values.name = creatorName;
-//    final newUser = await queryCreateUser.insert();
-//
-//    final queryCreatePresentation = Query<PresentationTable>(managedContext)
-//      ..values.url = presentationUrl;
-//    final newPresentation = await queryCreatePresentation.insert();
-//
-//    final queryCreateEvent = Query<EventTable>(managedContext)
-//      ..values.name = eventName
-//      ..values.id_code = eventId
-//      ..values.presentation = newPresentation
-//      ..values.presenter = newUser;
-//    await queryCreateEvent.insert();
+    final queryCreateUser = Query<UserTable>(managedContext)
+      ..values.name = presenterName;
+    final newUser = await queryCreateUser.insert();
+
+    final queryCreatePresenter = Query<PresenterTable>(managedContext)
+      ..values.user = newUser;
+    final newPresenter = await queryCreatePresenter.insert();
+
+    final queryCreatePresentation = Query<PresentationTable>(managedContext)
+      ..values.url = presentationUrl;
+    final newPresentation = await queryCreatePresentation.insert();
+
+    final queryCreateEvent = Query<EventTable>(managedContext)
+      ..values.name = eventName
+      ..values.id_code = eventId
+      ..values.presentation = newPresentation
+      ..values.presenter = newPresenter;
+    await queryCreateEvent.insert();
   }
 
   Future<bool> existsEvent(String eventId) async {
-//    final querySelectEvent = Query<EventTable>(managedContext)
-//        ..where((EventTable event) => event.id_code).equalTo(eventId);
-//    final event = await querySelectEvent.fetchOne();
-//    return event != null;
-    return null;
+    final querySelectEvent = Query<EventTable>(managedContext)
+        ..where((EventTable event) => event.id_code).equalTo(eventId);
+    final event = await querySelectEvent.fetchOne();
+    return event != null;
   }
 
   Future<Map<String, Object>> getEventInfo(String eventId) async {
-//    final querySelectEvent = Query<EventTable>(managedContext)
-//      ..where((EventTable event) => event.id_code).equalTo(eventId);
-//    final event = await querySelectEvent.fetchOne();
-//    final presentationId = event.presentation.id;
-//
-//    final querySelectPresentation = Query<PresentationTable>(managedContext)
-//      ..where((PresentationTable presentation) => presentation.id).equalTo(presentationId);
-//    final presentation = await querySelectPresentation.fetchOne();
-//
-//    return <String, Object>{
-//      'event_name': event.name,
-//      'presentation_url': presentation.url,
-//    };
-    return null;
+    final querySelectEvent = Query<EventTable>(managedContext)
+      ..where((EventTable event) => event.id_code).equalTo(eventId);
+    final event = await querySelectEvent.fetchOne();
+
+    final querySelectPresentation = Query<PresentationTable>(managedContext)
+      ..where((PresentationTable presentation) => presentation.id).equalTo(event.presentation.id);
+    final presentation = await querySelectPresentation.fetchOne();
+
+    return <String, Object>{
+      'event_name': event.name,
+      'presentation_url': presentation.url,
+    };
   }
 
-  Future<Map<String, Object>> joinEvent(String eventId, String userName) async {
-//    final querySelectEvent = Query<EventTable>(managedContext)
-//      ..where((EventTable event) => event.id_code).equalTo(eventId);
-//    final event = await querySelectEvent.fetchOne();
-//
-//    final queryCreateUser = Query<UserTable>(managedContext)
-//      ..values.name = userName
-//      ..values.event = event;
-//    final newUser = await queryCreateUser.insert();
-//
-//    final querySelectPresentation = Query<PresentationTable>(managedContext)
-//      ..where((PresentationTable presentation) => presentation.id).equalTo(event.presentation.id);
-//    final presentation = await querySelectPresentation.fetchOne();
-//
-//    return <String, Object>{
-//      'user_id': newUser.id,
-//      'event_name': event.name,
-//      'presentation_url': presentation.url,
-//    };
-    return null;
+  Future<Map<String, Object>> joinEvent(String eventId, String listenerName) async {
+    final querySelectEvent = Query<EventTable>(managedContext)
+      ..where((EventTable event) => event.id_code).equalTo(eventId);
+    final newEvent = await querySelectEvent.fetchOne();
+
+    final queryCreateUser = Query<UserTable>(managedContext)
+      ..values.name = listenerName;
+    final newUser = await queryCreateUser.insert();
+
+    final queryCreateListener = Query<ListenerTable>(managedContext)
+      ..values.user = newUser
+      ..values.event = newEvent;
+    await queryCreateListener.insert();
+
+    final querySelectPresentation = Query<PresentationTable>(managedContext)
+      ..where((PresentationTable presentation) => presentation.id).equalTo(newEvent.presentation.id);
+    final presentation = await querySelectPresentation.fetchOne();
+
+    return <String, Object>{
+      'user_id': newUser.id,
+      'event_name': newEvent.name,
+      'presentation_url': presentation.url,
+    };
   }
 
   Future<void> newMessage(int userId, String text, bool isQuestion) async {
-//    final queryCreateMessage = Query<MessageTable>(managedContext)
-//      ..values.text = text
-//      ..values.is_question = isQuestion
-//      ..values.user.id = userId
-//      ..values.time = DateTime.now().millisecondsSinceEpoch.toString();
-//    await queryCreateMessage.insert();
+    final queryCreateMessage = Query<MessageTable>(managedContext)
+      ..values.text = text
+      ..values.is_question = isQuestion
+      ..values.user.id = userId
+      ..values.time = DateTime.now().millisecondsSinceEpoch;
+    await queryCreateMessage.insert();
   }
 
-  Future<void> removeEventData(String eventIdCode) async {
-//    final querySelectEvent = Query<EventTable>(managedContext)
-//      ..where((EventTable event) => event.id_code).equalTo(eventIdCode);
-//    final event = await querySelectEvent.fetchOne();
-//    final eventId = event.id;
-//    final presentation = event.presentation;
-//    final presenter = event.presenter;
-//    await querySelectEvent.delete();
-//
-//    final querySelectPresenter = Query<UserTable>(managedContext)
-//      ..where((UserTable user) => user.id).equalTo(presenter.id);
-//    await querySelectPresenter.delete();
-//
-//    final querySelectPresentation = Query<PresentationTable>(managedContext)
-//      ..where((PresentationTable presentation) => presentation.id).equalTo(presentation.id);
-//    await querySelectPresentation.delete();
-//
-//    final querySelectListeners = Query<UserTable>(managedContext)
-//      ..where((UserTable user) => user.event.id).equalTo(eventId);
-//    await querySelectListeners.delete();
+  Future<void> removeEvent(String eventIdCode) async {
+    final querySelectEvent = Query<EventTable>(managedContext)
+      ..where((EventTable event) => event.id_code).equalTo(eventIdCode);
+    await querySelectEvent.delete();
   }
 
   Future<Map<String, Object>> getEventMessages(String eventIdCode) async {
-//    final querySelectEvent = Query<EventTable>(managedContext)
-//      ..where((EventTable event) => event.id_code).equalTo(eventIdCode);
-//    final event = await querySelectEvent.fetchOne();
-//    final eventId = event.id;
-//
-//    final querySelectListeners = Query<UserTable>(managedContext)
-//      ..where((UserTable user) => user.event.id).equalTo(eventId);
-//    final listeners = await querySelectListeners.fetch();
-//
-//    var messages = <Map<String, Object>>[];
-//    for (var listener in listeners) {
-//      final querySelectMessages = Query<MessageTable>(managedContext)
-//        ..where((MessageTable message) => message.user.id).equalTo(listener.id);
-//      messages.addAll((await querySelectMessages.fetch()).map((message) => <String, Object>{
-//        'user_name': listener.name,
-//        'text': message.text,
-//        'is_question': message.is_question,
-//      }).toList());
-//    }
-//
-//    return <String, Object>{
-//      'messages': messages,
-//    };
-    return null;
+    final querySelectEvent = Query<EventTable>(managedContext)
+      ..where((EventTable event) => event.id_code).equalTo(eventIdCode);
+    final event = await querySelectEvent.fetchOne();
+
+    final querySelectListener = Query<ListenerTable>(managedContext)
+      ..where((ListenerTable listener) => listener.event.id).equalTo(event.id);
+    final listeners = await querySelectListener.fetch();
+
+    final querySelectPresenter = Query<PresenterTable>(managedContext)
+      ..where((PresenterTable presenter) => presenter.id).equalTo(event.presenter.id);
+    final presenter = await querySelectPresenter.fetchOne();
+
+    final userIds = listeners.map((listener) => listener.user.id).toList()
+      ..add(presenter.user.id);
+
+    final querySelectUser = Query<UserTable>(managedContext)
+      ..where((UserTable user) => user.id).oneOf(userIds);
+    final users = await querySelectUser.fetch();
+
+    var messages = <Map<String, Object>>[];
+    for (var user in users) {
+      final querySelectMessage = Query<MessageTable>(managedContext)
+        ..where((MessageTable message) => message.user.id).equalTo(user.id);
+      final userMessages = await querySelectMessage.fetch();
+      messages.addAll(userMessages.map((message) => <String, Object>{
+        'user_name': user.name,
+        'text': message.text,
+        'is_question': message.is_question,
+      }));
+    }
+
+    return <String, Object>{
+      'messages': messages,
+    };
   }
 
 }
