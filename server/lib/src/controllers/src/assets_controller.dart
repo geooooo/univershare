9be636e6e697f5cead55d9287c9befe755128d1c
yury kornienko <geo97it@gmail.com>
2019-06-08 17:06:55 +0300

    fix server

---
+++ server/lib/src/controllers/src/assets_controller.dart
@@ -0,0 +1,24 @@
+import 'package:aqueduct/aqueduct.dart';
+import 'package:server/src/internal/di_injector.dart';
+import 'package:api_models/api_models.dart' as api_models;
+
+class JoinEventController extends ResourceController {
+
+  final DiInjector _diInjector;
+
+  JoinEventController(this._diInjector);
+
+  @Operation.post()
+  Future<Response> joinEvent(@Bind.body() Object request) async {
+    final requestData = api_models.JoinEventRequest.fromMap(request);
+
+    _diInjector.logger.logRestApi(this.request.method, this.request.path.string, requestData.asMap());
+
+    final responseData = await _diInjector.db.joinEvent(requestData.eventId, requestData.userName);
+    final response = api_models.JoinEventResponse.fromMap(responseData)
+      ..presentationUrl = _diInjector.common.getFullUrl(responseData['presentation_url'])
+      ..status = 0;
+
+    return Response.ok(response.asMap());
+  }
+}
